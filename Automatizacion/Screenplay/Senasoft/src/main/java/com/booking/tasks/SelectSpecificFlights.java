5836fe8ca11aa1908c626e3424cce9160aaf2281
David Santiago Bulla <dsbulla8@soy.sena.edu.co>
2023-09-27 17:51:56 -0500

    finish the first scenario of flights module

--- Automatizacion/Screenplay/Senasoft/src/main/java/com/booking/tasks/SelectSpecificFlights.java
+++ Automatizacion/Screenplay/Senasoft/src/main/java/com/booking/tasks/SelectSpecificFlights.java
@@ -1,16 +1,30 @@
 package com.booking.tasks;
 
-import com.booking.userinterfaces.MenuService;
+import com.booking.userinterfaces.FlightPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.annotations.Subject;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class SelectSpecificFlights implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
-        //actor.attemptsTo(Click.on(MenuService.IconsService.of("2")));
+        //Enter data to search for flights, click on the search button
+        actor.attemptsTo(
+                Click.on(FlightPage.DROP_DOWN_FIRST_LOCATION),
+                Click.on(FlightPage.TXT_CITY_SELECTED),
+                Enter.theValue("bogota").into(FlightPage.TXT_ENTER_CITY),
+                Click.on(FlightPage.LBL_DESIRED_CITY),
+                Click.on(FlightPage.TXT_FINAL_CITY),
+                Enter.theValue("tolima").into(FlightPage.TXT_ENTER_CITY),
+                Click.on(FlightPage.LBL_DESIRED_CITY),
+                Click.on(FlightPage.TXT_DATES),
+                Click.on(FlightPage.DATE_FIRST_ELEMENT),
+                Click.on(FlightPage.DATE_SECOND_ELEMENT),
+                Click.on(FlightPage.BTN_SEARCH));
     }
     public static  SelectSpecificFlights searchFlightToDestination(){
         return instrumented(SelectSpecificFlights.class);
--- Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/stepsdefinitions/FlightsStepDefinition.java
+++ Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/stepsdefinitions/FlightsStepDefinition.java
@@ -1,27 +1,31 @@
 package com.booking.stepsdefinitions;
 
+import com.booking.questions.SearchSpecificFlight;
 import com.booking.tasks.ClickOnServiceButton;
+import com.booking.tasks.SelectSpecificFlights;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
+import org.hamcrest.Matchers;
 
 public class FlightsStepDefinition {
-
-
+    //application of steps for the flight search scenario for a specific destination
     @Given("that the user is on the {string} page")
     public void thatTheUserIsOnThePage(String option) {
-        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url("https://www.booking.com/"));
+        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url("https://www.booking.com"));
         OnStage.theActorInTheSpotlight().wasAbleTo(ClickOnServiceButton.clickOnServiceButton(option));
     }
 
     @When("the user enter the data for her search and clicks on search")
     public void theUserEnterTheDataForHerSearchAndClicksOnSearch() {
-
+        OnStage.theActorInTheSpotlight().attemptsTo(SelectSpecificFlights.searchFlightToDestination());
     }
     @Then("the user should see the results of their search")
     public void theUserShouldSeeTheResultsOfTheirSearch() {
-
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(SearchSpecificFlight.validateFlightsResults(),
+                Matchers.is(true)));
     }
 }
