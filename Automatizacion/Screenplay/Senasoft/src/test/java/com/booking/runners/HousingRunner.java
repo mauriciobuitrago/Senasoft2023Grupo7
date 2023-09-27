077d966c8aa65d9779036990a5d7716cfacfe89d
David Santiago Bulla <dsbulla8@soy.sena.edu.co>
2023-09-27 15:13:18 -0500

    save changes

--- Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/runners/HousingRunner.java
+++ Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/runners/HousingRunner.java
@@ -10,10 +10,7 @@ import org.junit.runner.RunWith;
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         glue = "com.booking.stepsdefinitions"
 )
-<<<<<<<< HEAD:Automatizacion/POM/BookingPOM/src/test/java/com/booking/runners/LoginRunner.java
-public class LoginRunner {
-========
+
 
 public class HousingRunner {
->>>>>>>> c411d7511a68eb63d1ea615a4cb7b3b7d9c3d62d:Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/runners/HousingRunner.java
 }
