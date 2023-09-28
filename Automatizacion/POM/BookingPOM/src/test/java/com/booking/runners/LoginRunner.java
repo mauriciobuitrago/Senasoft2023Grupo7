ae35c59ba90d08b2573343196c852498e46571de
David Santiago Bulla <dsbulla8@soy.sena.edu.co>
2023-09-28 07:18:27 -0500

    resolved problem

--- Automatizacion/POM/BookingPOM/src/test/java/com/booking/runners/LoginRunner.java
+++ Automatizacion/POM/BookingPOM/src/test/java/com/booking/runners/LoginRunner.java
@@ -6,14 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberSerenityRunner.class)
 @CucumberOptions(
-        features = "src/test/resources/features/housing.feature",
+        features = "src/test/resources/features/login.feature",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         glue = "com.booking.stepsdefinitions"
 )
-<<<<<<<< HEAD:Automatizacion/POM/BookingPOM/src/test/java/com/booking/runners/LoginRunner.java
-public class LoginRunner {
-========
 
-public class HousingRunner {
->>>>>>>> c411d7511a68eb63d1ea615a4cb7b3b7d9c3d62d:Automatizacion/Screenplay/Senasoft/src/test/java/com/booking/runners/HousingRunner.java
-}
+public class LoginRunner{}
+
+
